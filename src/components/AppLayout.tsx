fec918e68dca6f60764ed605c63096bd9924f529
dougcpr <doug.cpr@gmail.com>
2023-04-14 13:38:33 -0700

    initial setup for astro project with supabase

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,16 +2,10 @@ import React, {FC, useEffect, useState} from 'react';
 import {useRouter} from "next/router";
 import styled from 'styled-components';
 import LogOut from "@geist-ui/icons/logOut";
-import {Button, Modal, Select} from "@geist-ui/core";
-import {Inbox, Settings} from "@geist-ui/icons";
+import {Button} from "@geist-ui/core";
 import {Session, useSupabaseClient} from "@supabase/auth-helpers-react";
 import {AuthChangeEvent} from "@supabase/gotrue-js";
 import Home from "@geist-ui/icons/home";
-import Plus from "@geist-ui/icons/plus";
-import {useFormik} from "formik";
-import {Employee, Ticket, TicketPriorities} from "@/features/ticket/models";
-import {PostgrestResponse} from "@supabase/supabase-js";
-import {TextArea} from "@/components/core/TextArea";
 
 const NavBarContainer = styled.div`
   display: grid;
@@ -43,11 +37,6 @@ const SideNavBarOperationalButtons = styled.div`
   padding-bottom: 3rem;
 `
 
-const CreateTicketSelect = styled(Select)`
-  background-color: #323343;
-  color: #908fab;
-`
-
 type AppLayoutProps = {
   children: any;
 };
@@ -56,21 +45,7 @@ const AppLayout: FC<AppLayoutProps> = ({children}) => {
   const supabase = useSupabaseClient()
   const router = useRouter()
   const [session, setSession] = useState<Session | null>(null)
-  const [state, setState] = useState(false)
-  const [users, setUsers] = useState<any>()
 
-  const handler = () => setState(true)
-  const closeHandler = () => {
-    setState(false)
-  }
-  useEffect(() => {
-    supabase
-      .from("Employees")
-      .select("*")
-      .then(({data}: PostgrestResponse<any>) => {
-        setUsers(data)
-      })
-  }, [])
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }: any) => {
       setSession(session)
@@ -91,31 +66,6 @@ const AppLayout: FC<AppLayoutProps> = ({children}) => {
       return "transparent"
     }
   }
-
-  const formik = useFormik({
-    initialValues: {
-      title: '',
-      description: '',
-      status: 'Todo',
-      linkedTickets: [],
-      priority: TicketPriorities.Low,
-      assignedTo: 3,
-      ticketType: 'task'
-    },
-    onSubmit: async (values: Partial<Ticket>) => {
-      try {
-        const {data} = await supabase
-          .from('Tickets')
-          .insert([
-            values,
-          ])
-      } catch (err) {
-        console.error(err)
-      } finally {}
-      setState(false)
-      formik.handleReset({})
-    },
-  });
   async function signOut() {
     const { error } = await supabase.auth.signOut()
     await router.push('/')
@@ -127,18 +77,11 @@ const AppLayout: FC<AppLayoutProps> = ({children}) => {
           <SideNavBar>
             <SideNavBarButtonContainer>
               <SideNavBarNavigationButtons>
-                {/*TODO: MAKE LOOPED TEMPLATE OBJECT */}
-                <Button icon={<Plus color="#858699"/>} onClick={handler} auto scale={1} style={{border: 0}} >New Ticket</Button>
                 <Button
                   onClick={() => router.push('/')} style={{backgroundColor: determineButtonBackgroundColor('/'), border: 0}}
                   icon={<Home color="#858699"/>}
                   auto
                   scale={1}>Home</Button>
-                <Button
-                  onClick={() => router.push('/ticket-manager')} style={{backgroundColor: determineButtonBackgroundColor('/ticket-manager'), border: 0}}
-                  icon={<Inbox color="#858699" />}
-                  auto
-                  scale={1} >Inbox</Button>
               </SideNavBarNavigationButtons>
               <SideNavBarOperationalButtons>
                 <Button onClick={signOut} style={{backgroundColor: "transparent", border: 0}} icon={<LogOut />} auto scale={1}>Logout</Button>
@@ -147,44 +90,6 @@ const AppLayout: FC<AppLayoutProps> = ({children}) => {
           </SideNavBar>
           {children}
         </NavBarContainer>
-        <Modal width={2} visible={state} onClose={() => {closeHandler(); formik.resetForm();}}>
-          <Modal.Title>Create Ticket</Modal.Title>
-          <Modal.Content>
-            <form style={{display: "grid", gridTemplateRows: "1fr 1fr 1fr", gridRowGap: "1rem"}}>
-              <TextArea id="title" name="title" placeholder="Ticket title" onChange={formik.handleChange} value={formik.values.title}/>
-              <TextArea id="description" name="description" placeholder="Add description..." onChange={formik.handleChange} value={formik.values.description}/>
-              <div style={{display: 'grid', gridTemplateColumns: "repeat(4, 1fr)", gridColumnGap: "0.5rem"}}>
-                <CreateTicketSelect pure={true} value={formik.values.status} onChange={(v: string | string[]) => formik.values.status = v}>
-                  <Select.Option value="Todo">Todo</Select.Option>
-                  <Select.Option value="In Progress">In Progress</Select.Option>
-                  <Select.Option value="Waiting on Engineering">Waiting on Engineering</Select.Option>
-                  <Select.Option value="Waiting on Customer">Waiting on Customer</Select.Option>
-                  <Select.Option value="Closed">Closed</Select.Option>
-                </CreateTicketSelect>
-                <CreateTicketSelect pure={true} placeholder="Priority" onChange={(v: any) => formik.values.priority = v} value={formik.values.priority}>
-                  <Select.Option value={TicketPriorities.Low}>{TicketPriorities.Low}</Select.Option>
-                  <Select.Option value={TicketPriorities.Medium}>{TicketPriorities.Medium}</Select.Option>
-                  <Select.Option value={TicketPriorities.High}>{TicketPriorities.High}</Select.Option>
-                  <Select.Option value={TicketPriorities.Critical}>{TicketPriorities.Critical}</Select.Option>
-                </CreateTicketSelect>
-                <CreateTicketSelect pure={true} placeholder="Type" onChange={(v: string | string[]) => formik.values.ticketType = v} value={formik.values.ticketType}>
-                  <Select.Option value="task">Task</Select.Option>
-                  <Select.Option value="bug">Bug</Select.Option>
-                  <Select.Option value="improvement">Improvement</Select.Option>
-                </CreateTicketSelect>
-                <CreateTicketSelect pure={true} placeholder="Assignee" onChange={(v: any) => formik.values.assignedTo = v} value={formik.values.assignedTo?.toString()}>
-                  {users && users.map((user: Employee) => {
-                    return (
-                      <Select.Option key={user.id} value={user.id?.toString()}>{user.name}</Select.Option>
-                    )
-                  })}
-                </CreateTicketSelect>
-              </div>
-            </form>
-          </Modal.Content>
-          <Modal.Action passive onClick={() => setState(false)}>Cancel</Modal.Action>
-          <Modal.Action onClick={() => formik.handleSubmit()}>Submit</Modal.Action>
-        </Modal>
       </>
     )
   } else {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,8 @@ import React, {useEffect, useState} from "react";
 import {Session, useSupabaseClient} from '@supabase/auth-helpers-react'
 import SignIn from "@/pages/sign-in";
 import {AuthChangeEvent} from "@supabase/gotrue-js";
-import {Bar, BarChart, ResponsiveContainer, XAxis, YAxis} from "recharts";
-import {supabase} from "@/lib/supabaseClient";
 const Index = () => {
   const [session, setSession] = useState<Session | null>(null)
-  const [openTickets, setOpenTickets] = useState([])
-  const [closedTickets, setClosedTickets] = useState([])
   const [isLoading, setLoading] = useState(true)
   const supabase = useSupabaseClient()
   useEffect(() => {
@@ -23,44 +19,6 @@ const Index = () => {
     return () => subscription.unsubscribe()
   }, [])
 
-  useEffect(() => {
-    fetchOpenTicketList()
-    fetchClosedTicketList()
-  }, [])
-
-  async function fetchOpenTicketList() {
-    let { data }: any  = await supabase
-      .from('Tickets')
-      .select(`
-        status
-      `)
-      .neq('status', 'Closed')
-      .order('created_at')
-    if (data) setOpenTickets(data)
-  }
-
-  async function fetchClosedTicketList() {
-    let { data }: any  = await supabase
-      .from('Tickets')
-      .select(`
-        status
-      `)
-      .eq('status', 'Closed')
-      .order('created_at')
-    if (data) setClosedTickets(data)
-  }
-
-  const data = [
-    {
-      name: 'Open',
-      amt: openTickets?.length,
-    },
-    {
-      name: 'Closed',
-      amt: closedTickets?.length,
-    }
-  ];
-
   if (isLoading) return <div />
 
   return (
@@ -68,15 +26,7 @@ const Index = () => {
       {!session ? (
         <SignIn setSession={setSession} />
       ) : (
-        <div style={{padding: "2rem"}}>
-          <ResponsiveContainer width="30%" height="30%">
-            <BarChart data={data}>
-              <Bar dataKey="amt" fill="#82ca9d" />
-              <XAxis dataKey="name" />
-              <YAxis dataKey="amt" />
-            </BarChart>
-          </ResponsiveContainer>
-        </div>
+       <div>Hello Main Page</div>
       )}
     </>
   )
